efc9bb5a26fa06a90823e09b0ffda15c34b0d963
NachtRaben <stilleraben@gmail.com>
2017-04-12 18:47:00 -0400

    Add diax appender and manually apply formatting for now.

--- src/main/java/me/diax/bot/lib/util/DiaxDiscordAppender.java
+++ src/main/java/me/diax/bot/lib/util/DiaxDiscordAppender.java
@@ -1,8 +1,13 @@
 package me.diax.bot.lib.util;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import me.diax.bot.DiaxBot;
 import net.dv8tion.jda.core.entities.TextChannel;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Comportment on 10/04/2017.
@@ -15,9 +20,18 @@ public class DiaxDiscordAppender<E> extends AppenderBase<E> {
 
     @Override
     protected void append(E eventObject) {
-        TextChannel channel = DiaxBot.SHARDS[DiaxUtil.getShard(GUILD_ID)].getGuildById(GUILD_ID).getTextChannelById(CHANNEL_ID);
-        for (String string : DiaxUtil.splitStringEvery(eventObject + "", 2000)) {
-            channel.sendMessage(string).queue();
+        if (eventObject instanceof ILoggingEvent) {
+            ILoggingEvent event = (ILoggingEvent) eventObject;
+            TextChannel channel = DiaxBot.SHARDS[DiaxUtil.getShard(GUILD_ID)].getGuildById(GUILD_ID).getTextChannelById(CHANNEL_ID);
+            Date time = new Date(event.getTimeStamp());
+            DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+            String loggerName = event.getLoggerName();
+            if (loggerName.contains(".") && loggerName.lastIndexOf("." + 1) < loggerName.length())
+                loggerName = loggerName.substring(loggerName.lastIndexOf(".") + 1);
+
+            String eventString = "[" + formatter.format(time) + "] [" + event.getLevel() + "] [" + loggerName + "] " + event.getFormattedMessage();
+            for (String string : DiaxUtil.splitStringEvery(eventString + "", 2000))
+                channel.sendMessage(string).queue();
         }
     }
 }
