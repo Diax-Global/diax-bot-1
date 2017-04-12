8e309ae422ebc40f53d10b1455723c1a1cacf75b
Comportment <Comportment@protonmail.com>
2017-04-12 23:02:10 +0100

    Kick command anyone?

--- src/main/java/me/diax/bot/commands/administrative/KickCommand.java
+++ src/main/java/me/diax/bot/commands/administrative/KickCommand.java
@@ -11,17 +11,17 @@ import net.dv8tion.jda.core.exceptions.PermissionException;
  * Created by Comportment on 12/04/2017.
  * If you don't understand this, we are screwed.
  */
-@DiaxCommandDescription(triggers = {"ban", "banne", "banné"}, guildOnly = true, minimumArgs = 1, permission = Permission.BAN_MEMBERS)
-public class BanCommand extends DiaxCommand {
+@DiaxCommandDescription(triggers = {"kick"}, guildOnly = true, permission = Permission.KICK_MEMBERS)
+public class KickCommand extends DiaxCommand {
 
     @Override
     public void execute(Message trigger, String args) {
         trigger.getMentionedUsers().forEach(user -> {
             try {
-                trigger.getGuild().getController().ban(trigger.getGuild().getMember(user), 7).queue();
-                trigger.getChannel().sendMessage(DiaxUtil.simpleEmbed(DiaxUtil.makeName(user) + "has been banned!.").build()).queue();
+                trigger.getGuild().getController().kick(trigger.getGuild().getMember(user)).queue();
+                trigger.getChannel().sendMessage(DiaxUtil.simpleEmbed(DiaxUtil.makeName(user) + "has been kicked!").build()).queue();
             } catch (PermissionException exception) {
-                trigger.getChannel().sendMessage(DiaxUtil.errorEmbed("I could not ban " + DiaxUtil.makeName(user))).queue();
+                trigger.getChannel().sendMessage(DiaxUtil.errorEmbed("I could not kick " + DiaxUtil.makeName(user))).queue();
             }
         });
     }
