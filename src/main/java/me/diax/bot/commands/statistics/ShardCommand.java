9920e94edcf1ec4e910dc253e33f061d8aa34065
NachtRaben <stilleraben@gmail.com>
2017-04-12 16:42:22 -0400

    Fixed sharding logic and updated shard command with proper logic.

--- src/main/java/me/diax/bot/commands/statistics/ShardCommand.java
+++ src/main/java/me/diax/bot/commands/statistics/ShardCommand.java
@@ -1,9 +1,13 @@
 package me.diax.bot.commands.statistics;
 
+import me.diax.bot.DiaxBot;
 import me.diax.bot.lib.command.DiaxCommand;
 import me.diax.bot.lib.command.DiaxCommandDescription;
 import me.diax.bot.lib.util.DiaxUtil;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Comportment on 11/04/2017.
@@ -12,8 +16,14 @@ import net.dv8tion.jda.core.entities.Message;
 @DiaxCommandDescription(triggers = {"shard"})
 public class ShardCommand extends DiaxCommand {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShardCommand.class);
+
     @Override
     public void execute(Message trigger, String args) {
-        trigger.getChannel().sendMessage(DiaxUtil.simpleEmbed(String.format("♦ Shard: [%s/%s]", trigger.getGuild() == null ? 0 : DiaxUtil.getShard(trigger.getGuild()), trigger.getJDA().getShardInfo().getShardTotal())).build()).queue();
+        JDA.ShardInfo shardInfo = trigger.getJDA().getShardInfo();
+        trigger.getChannel().sendMessage(DiaxUtil.simpleEmbed(String.format("♦ Shard: [%s/%s]",
+                shardInfo != null ? shardInfo.getShardId() : 1,
+                shardInfo != null ? shardInfo.getShardTotal() : DiaxBot.SHARDS.length
+        )).build()).queue();
     }
 }
