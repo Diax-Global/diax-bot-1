188a57eb50d24b6a3df42c833f9e002f2a9c270e
Comportment <Comportment@protonmail.com>
2017-04-14 13:30:13 +0100

    Erm some dev descriptions [should add more]

--- src/main/java/me/diax/bot/commands/statistics/StatisticsCommand.java
+++ src/main/java/me/diax/bot/commands/statistics/StatisticsCommand.java
@@ -20,6 +20,6 @@ public class StatisticsCommand extends DiaxCommand {
     @Override
     public void execute(Message trigger, String args) {
         List<JDA> shards = Arrays.asList(DiaxBot.SHARDS);
-        trigger.getChannel().sendMessage(DiaxUtil.simpleEmbed(String.format("Shards: %s\nGuilds: %s\nUsers: %s", DiaxBot.SHARDS.length, shards.stream().flatMap(jda -> jda.getGuilds().stream().distinct()).count(), shards.stream().flatMap(jda -> jda.getUsers().stream().distinct()).count())).build()).queue();
+        trigger.getChannel().sendMessage(DiaxUtil.simpleEmbed(String.format("**Developers:**\n[Comportment#9489](https://github.com/Comportment)- Project lead, original bot design, command lib, commands.\n[Crystal#3166](https://github.com/CrystalMare)- Backend, Injection, Diax Service.\n[Nomsy#7453](https://github.com/Truency)- Website developer and designer.\n[Reece#7982](https://github.com/ReeceyBoi81)- Database management.\n[NachtRaben#8307](https://github.com/NachtRaben)- Music logic, general bugfixes.\n\n ♦ Shards: %s\n\uD83D\uDC64 Guilds: %s\n\uD83D\uDCDD Channels: %s\n\uD83D\uDC65 Users: %s", DiaxBot.SHARDS.length, shards.stream().flatMap(jda -> jda.getGuilds().stream().distinct()).count(), shards.stream().flatMap(jda -> jda.getGuilds().stream()).mapToLong(guild -> guild.getVoiceChannels().size()).count(), shards.stream().flatMap(jda -> jda.getUsers().stream().distinct()).count())).build()).queue();
     }
 }
