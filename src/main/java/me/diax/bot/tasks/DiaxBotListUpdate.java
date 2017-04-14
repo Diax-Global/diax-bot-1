130c6c3459f7b2171e3358b37a276c0fa2e03113
Comportment <Comportment@protonmail.com>
2017-04-15 00:22:33 +0100

    Adding shard support to walshy's bot list.

--- src/main/java/me/diax/bot/tasks/DiaxBotListUpdate.java
+++ src/main/java/me/diax/bot/tasks/DiaxBotListUpdate.java
@@ -3,10 +3,12 @@ package me.diax.bot.tasks;
 import com.mashape.unirest.http.Unirest;
 import me.diax.bot.DiaxBot;
 import me.diax.bot.lib.util.scheduler.DiaxTask;
-import org.json.JSONArray;
+import net.dv8tion.jda.core.JDA;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Comportment on 14/04/2017.
@@ -17,11 +19,14 @@ public class DiaxBotListUpdate extends DiaxTask {
     public void run() {
         try {
             JSONObject object;
-            if (DiaxBot.SHARDS.length >= 1) {
+            if (DiaxBot.SHARDS.length > 1) {
                 object = new JSONObject().put("server_count", Arrays.stream(DiaxBot.SHARDS).mapToLong(jda -> jda.getGuilds().size()).sum());
             } else {
-                object = null;
-                //object = new JSONObject().put("shards", new JSONArray().put(Arrays.stream(DiaxBot.SHARDS)));
+                List<Integer> amount = new ArrayList<>();
+                for (JDA jda : DiaxBot.SHARDS) {
+                    amount.add(jda.getGuilds().size());
+                }
+                object = new JSONObject().put("shards", amount);
             }
             DiaxBot.LOGGER.info("Update post returned: " + Unirest.post("https://walshydev.com/bot-list/api/bots/295500621862404097/stats")
                     .header("Authorization", "kUzuzrgDDTLUaR.QEBDwPgnCEpUEvZPFQlZnDdQdWzYXnWKRukkgqxJUNAwfwiLktIxWLl.EKWyXPfMgqWnGrWycuViRaT-tzvnh")
